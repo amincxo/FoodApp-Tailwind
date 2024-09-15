9f3152adac56d4c7f40409bcc063a0a7ec658b87
unknown <amincxo@gmail.com>
2024-09-15 22:37:58 +0330

    menu section complate picker

--- src/main.js
+++ src/main.js
@@ -17,3 +17,46 @@ closeIcon.addEventListener('click' , () =>{
 hamburger.addEventListener('click' , ()=> {
         navMenu.classList.remove('hidden')
 })
+
+const tabs = document.querySelectorAll(".tabs_warp ul li");
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const beverage = document.querySelectorAll(".beverage");
+const snacks = document.querySelectorAll(".snack");
+
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(tab => {
+            tab.classList.remove("active");
+        })
+
+        tab.classList.add("active");
+
+        const tabval =  tab.getAttribute('data-tabs');
+
+        all.forEach(item => {
+            item.style.display = 'none'
+        })
+
+        if(tabval == 'food') {
+            foods.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+        else if (tabval == 'snack') {
+            snacks.forEach(item => {
+                item.style.display = 'block'
+            })
+        } else if (tabval == 'beverage') {
+            beverage.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+        else {
+            all.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+    })
+})
