0156bfeb01410f62b2b7ca77896eb2595a9b0ac1
unknown <amincxo@gmail.com>
2024-09-16 19:47:52 +0330

    dark mode complate

--- src/main.js
+++ src/main.js
@@ -60,3 +60,34 @@ tabs.forEach(tab => {
         }
     })
 })
+
+
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if(localStorage.getItem('mode') == 'dark'){
+    darkMode();
+}else {
+    lightMode();
+}
+
+themeBtn.addEventListener('click', (e)=> {
+    if (localStorage.getItem('mode') == 'light') {
+        darkMode();
+    }else{
+    lightMode();
+}
+} )
+
+function darkMode (){
+    html.classList.add("dark");
+    themeBtn.classList.replace('ri-moon-line', 'ri-sun-line');
+    localStorage.setItem('mode','dark')
+}
+
+
+function lightMode () {
+    html.classList.remove("dark");
+    themeBtn.classList.replace('ri-sun-line', 'ri-moon-line');
+    localStorage.setItem('mode','light')
+}
